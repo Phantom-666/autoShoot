aff57ea3bd145f2fd1f6374150a2a0f7065a051e
Demon <v@mail.ru>
2021-06-25 23:26:26 +0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
 import https from 'https'
 import http from 'http'
-import {takeUrl} from './takeUrl'
-import {action} from './request'
+import { takeUrl } from './takeUrl'
+import { action } from './request'
 
 type urlType = {
-    protocol :string,
-    url : string,
-    count : number
+  protocol: string
+  url: string
+  count: number
 }
 const url = takeUrl(process.argv)
 
-const callback = (body:string) => {
-    const data = JSON.parse(body)
-    console.log(data)
+const callback = (body: string) => {
+  const data = JSON.parse(body)
+  console.log(data)
 }
 
-const request = (url:urlType) => {
-    if (url.protocol === 'http') {
-        action(http, url.url, callback)
-    }
-    if (url.protocol === 'https') {
+const request = (url: urlType) => {
+  switch (url.protocol) {
+    case 'http':
+      action(http, url.url, callback)
+      break
 
-        for (let i = 0; i < url.count; i++) {
-            action(https, url.url, callback)
-        }
-    }
+    case 'https':
+      for (let i = 0; i < url.count; i++) action(https, url.url, callback)
+      break
+  }
 }
 
 request(url)
